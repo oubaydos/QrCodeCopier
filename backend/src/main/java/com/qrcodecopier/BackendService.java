d18b3e28693ebb872d30357765ba488cf4c69fee
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-08 23:22:55 +0100

    fixed todo

--- backend/src/main/java/com/qrcodecopier/BackendService.java
+++ backend/src/main/java/com/qrcodecopier/BackendService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 
+import static com.qrcodecopier.Utils.verifySessionIdHash;
+
 @Singleton
 @RequiredArgsConstructor
 @Slf4j
@@ -16,8 +18,7 @@ public class BackendService {
 
     public HttpResponse<?> copyUrl(String token, String url) {
         validateToken(token);
-        // todo token should not be sessionId, you should hash it
-        broadcaster.broadcastAsync(url, session -> session.getId().equals(token));
+        broadcaster.broadcastAsync(url, session -> verifySessionIdHash(token, session.getId()));
         return HttpResponse.ok();
     }
 
