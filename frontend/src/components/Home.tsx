ff33c024933cc0383f6ddded931666615659aaed
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-13 23:30:14 +0100

    added qrcode

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,18 +2,21 @@ import React, {useEffect, useState} from "react";
 import "../style/Home.css"
 import {BACKEND_SOCKET_URL} from "../config/config";
 import {handleMessage} from "../service/websocket";
+import QRCode from "react-qr-code";
 
 
 function Home() {
     const [data, setData] = useState<Array<string>>([]);
+    const [sessionId, setSessionId] = useState<string>("")
     useEffect(() => {
         const webSocket = new WebSocket(BACKEND_SOCKET_URL);
-        webSocket.onmessage = handleMessage(webSocket, setData)
+        webSocket.onmessage = handleMessage(webSocket, setData, setSessionId)
     }, [])
 
 
     return (
         <div>
+            {sessionId && <QRCode value={sessionId} style={{margin: "20px"}}/>}
             {data.map(value => <h1 key={value}>{value}</h1>)}
         </div>
     );
--- frontend/src/service/websocket.ts
+++ frontend/src/service/websocket.ts
@@ -1,13 +1,13 @@
-import {redirect, showToken, showUrlToRedirectTo} from "./utils";
+import {redirect, showUrlToRedirectTo} from "./utils";
 import {Message, MessageType} from "./messageType";
 import {Dispatch, SetStateAction} from "react";
 
 export const handleMessage =
-    (webSocket: WebSocket, setData: Dispatch<SetStateAction<string[]>>) =>
+    (webSocket: WebSocket, setData: Dispatch<SetStateAction<string[]>>, setSessionId: Dispatch<SetStateAction<string>>) =>
         function handleUrlMessage(msg: { data: string }) {
             const message: Message = JSON.parse(msg.data);
             if (message.type === MessageType.SESSION_ID)
-                showToken(msg.data, setData)
+                setSessionId(message.message)
             else if (message.type === MessageType.REDIRECTION_URL) {
                 webSocket.close()
                 showUrlToRedirectTo(message.message, setData)
