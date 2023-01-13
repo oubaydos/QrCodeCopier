7c4a697a94deead2756cabadbf80962cddf69ab7
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-13 23:30:24 +0100

    remove unused code

--- frontend/src/service/utils.ts
+++ frontend/src/service/utils.ts
@@ -5,9 +5,6 @@ export const redirect = (url: string): void => {
     window.location.href = getUrlWithHttpOrDefault(url)
 }
 
-export function showToken(token: string, setData: Dispatch<SetStateAction<string[]>>) {
-    setData(data => [...data, "received token: " + token]);
-}
 
 export function showUrlToRedirectTo(url: string, setData: Dispatch<SetStateAction<string[]>>) {
     setData(data => [...data, "redirecting to: " + url]);
